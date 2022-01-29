555499f7e59448945ffb3da7fb7f3a0b8a275826
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-01-29 10:38:12 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
+
+
+// import React from 'react'
+// import ReactDom from 'react-dom'
+// ReactDom.render(
+//   <div>
+//     <h1>Assalamualikum Sir</h1>
+//     <h1>Good Morning</h1>
+//     <h1> <span>My Name Is :</span> <span>Muhammad Jamshed </span> </h1>
+//     <h1> <span>My Father Name Is :</span> <span>Shahid Mehmood </span> </h1>
+//     <h1> <span> :</span> <span> </span> </h1>
+//   </div>,
+//   document.getElementById('root')
+  
+// )
